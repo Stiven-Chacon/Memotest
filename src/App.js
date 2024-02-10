e3c8de0a7400c937516830501830c04647145bbc
coco <stivenchacon031@gmail.com>
2024-02-10 17:35:48 -0500

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import React from 'react';
+const imagenes = [
+  "https://icongr.am/devicon/angularjs-original.svg?size=128&color=currentColor",
+  "https://icongr.am/devicon/react-original-wordmark.svg?size=128&color=currentColor",
+  "https://icongr.am/devicon/bower-original.svg?size=128&color=currentColor",
+  "https://icongr.am/devicon/chrome-original.svg?size=128&color=currentColor",
+  "https://icongr.am/devicon/csharp-original.svg?size=128&color=currentColor",
+  "https://icongr.am/devicon/docker-original.svg?size=128&color=currentColor",
+  "https://icongr.am/devicon/django-original.svg?size=128&color=currentColor",
+  "https://icongr.am/devicon/google-original-wordmark.svg?size=128&color=currentColor",
+  "https://icongr.am/devicon/javascript-original.svg?size=128&color=currentColor",
+  "https://icongr.am/devicon/typescript-original.svg?size=128&color=currentColor"
+].flatMap((img) => [`a|${img}`, `b|${img}`]).sort(() => Math.random() - 0.5);
 
-function App() {
+export default function App() {
+  const [adivinado, setAdivinado] = useState([]);
+  const [Seleccionado, setSeleccionado] = useState([]);
+
+  useEffect(() => {
+    if (Seleccionado.length === 2) {
+      if (Seleccionado[0].split("|")[1] === Seleccionado[1].split("|")[1]) {
+        setAdivinado((adivinado) => adivinado.concat(Seleccionado))
+      }
+      setTimeout(() => {
+        setSeleccionado([])
+      }, 1000);
+    }
+  }, [Seleccionado])
+
+  useEffect(() => {
+    if (adivinado.length === imagenes.length) {
+      alert("Felicidades haz gando este juego!");
+      window.location.reload();
+    }
+  }, [adivinado])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{textAlign:'center', fontSize:'40px', color:'white', margin:'50px'}}>MEMOTEST</h1>
+      <ul style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(120px, 1fr))', gap: '20px', marginBottom:'20px' }}>
+        {imagenes.map((item) => {
+          const [, url] = item.split("|");
+          return (
+            <li onClick={() => Seleccionado.length < 2 && setSeleccionado(Seleccionado => Seleccionado.concat(item))} key={item} style={{ listStyleType: 'none', padding: '12', border: '1px solid white', cursor: 'pointer' }}>
+              {Seleccionado.includes(item) || adivinado.includes(item) ? (
+                <img src={url} alt={url} />
+              ) : (
+                <img src="https://icongr.am/clarity/eye.svg?size=128&color=currentColor" alt="hidden" />
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
-
-export default App;
